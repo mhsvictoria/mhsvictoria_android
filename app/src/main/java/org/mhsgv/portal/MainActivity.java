6d9271da87a1a13e4b9806851f097d03851aa1b5
Ron Allan <ron_allan@yahoo.com>
2020-09-12 15:00:16 -0700

    Updated tabs

--- app/src/main/java/org/mhsgv/portal/MainActivity.java
+++ app/src/main/java/org/mhsgv/portal/MainActivity.java
@@ -3,6 +3,7 @@ package org.mhsgv.portal;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,18 +16,23 @@ import androidx.viewpager2.widget.ViewPager2;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     private String[] titles;
 
     TabLayout tabLayout;
     ViewPager2 viewPager;
+    ScrollView mainScrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().setStatusBarColor(getResources().getColor(R.color.background));
+        mainScrollView = findViewById(R.id.main_scroll_view);
+
         titles = getResources().getStringArray(R.array.form_names);
 
         viewPager = findViewById(R.id.view_pager);
@@ -38,13 +44,33 @@ public class MainActivity extends AppCompatActivity {
                     @SuppressLint("SetTextI18n")
                     @Override public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
                         tab.setText("" + (position + 1));
-                        tab.setCustomView(R.layout.tab_layout);
+                        tab.setCustomView(R.layout.tab_unselected_layout);
                         View customView = tab.getCustomView();
                         assert customView != null;
                         TextView tv = customView.findViewById(R.id.tab_number);
                         tv.setText("" + (position + 1));
+                        if(position == 0) {
+                            Objects.requireNonNull(tab.getCustomView()).setBackground(getResources().getDrawable(R.drawable.tab_background));
+                        }
                     }
                 }).attach();
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Objects.requireNonNull(tab.getCustomView()).setBackground(getResources().getDrawable(R.drawable.tab_background));
+            }
+
+            @SuppressLint("UseCompatLoadingForDrawables")
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                Objects.requireNonNull(tab.getCustomView()).setBackground(getResources().getDrawable(R.drawable.tab_background_unselected));
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                Objects.requireNonNull(tab.getCustomView()).setBackground(getResources().getDrawable(R.drawable.tab_background));
+            }
+        });
 
     }
 
