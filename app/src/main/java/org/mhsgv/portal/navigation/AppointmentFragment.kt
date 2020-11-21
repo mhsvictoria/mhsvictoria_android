e7b4575678bf57d7b2fc3e7ac7a5a438b3e3d769
Ron Allan <ron_allan@yahoo.com>
2020-11-21 06:51:32 -0800

    Menu

--- app/src/main/java/org/mhsgv/portal/navigation/AppointmentFragment.kt
+++ app/src/main/java/org/mhsgv/portal/navigation/AppointmentFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import org.mhsgv.portal.R
 
 class AppointmentFragment : Fragment() {
@@ -19,8 +19,7 @@ class AppointmentFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        appointmentViewModel =
-                ViewModelProviders.of(this).get(AppointmentViewModel::class.java)
+        appointmentViewModel = ViewModelProvider.NewInstanceFactory().create(AppointmentViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_form, container, false)
         val textView: TextView = root.findViewById(R.id.text_appointment)
         appointmentViewModel.text.observe(viewLifecycleOwner, Observer {
